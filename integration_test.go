0b469f2dc973d953d532f565eb3766556c96ca47
Markus Prettner <markus.prettner@outlook.com>
2020-12-03 18:17:23 +0100

    Combined verbose-only tests with no-dry-run tests

--- integration_test.go
+++ integration_test.go
@@ -18,11 +18,7 @@ import (
 const testFolder = "test_filesystem_root"
 const quickBrownFox = "The quick brown fox jumps over the lazy dog.\n"
 
-func prepareTestFolder(t *testing.T) {
-	prepareTestFolder2(t, false)
-}
-
-func prepareTestFolder2(t *testing.T, emptyDirectory bool) {
+func prepareTestFolder(t *testing.T, emptyDirectory bool) {
 	_, err := os.Stat(testFolder)
 	if !os.IsNotExist(err) {
 		err = os.RemoveAll(testFolder)
@@ -84,124 +80,6 @@ func makeCommandStream(command sendCommand, data []byte) []byte {
 	return append(header, data...)
 }
 
-func TestLinkStream(t *testing.T) {
-	prepareTestFolder(t)
-	data := []byte("\x0f\x00\x05\x00file2\x11\x00\x05\x00file1")
-	runProgram(t, makeCommandStream(BTRFS_SEND_C_LINK, data))
-
-	stat1, _ := os.Stat(path.Join(testFolder, "file1"))
-	stat2, _ := os.Stat(path.Join(testFolder, "file2"))
-	if !os.SameFile(stat1, stat2) {
-		t.Fatal("file1 and file2 are not the same")
-	}
-}
-
-func TestMkdirStream(t *testing.T) {
-	prepareTestFolder(t)
-	data := []byte("\x0f\x00\x04\x00dir2\x03\x00\x08\x00\xef\xcd\xab\x89\x67\x45\x23\x01")
-	runProgram(t, makeCommandStream(BTRFS_SEND_C_MKDIR, data))
-
-	stat, err := os.Stat(path.Join(testFolder, "dir2"))
-	if os.IsNotExist(err) || !stat.IsDir() {
-		t.Fatal("dir2 was not created")
-	}
-}
-
-func TestMkfileStream(t *testing.T) {
-	prepareTestFolder(t)
-	data := []byte("\x0f\x00\x05\x00file2\x03\x00\x08\x00\xef\xcd\xab\x89\x67\x45\x23\x01")
-	runProgram(t, makeCommandStream(BTRFS_SEND_C_MKFILE, data))
-
-	stat, err := os.Stat(path.Join(testFolder, "file2"))
-	if os.IsNotExist(err) || stat.IsDir() {
-		t.Fatal("file2 was not created")
-	}
-}
-
-func TestRenameStream(t *testing.T) {
-	prepareTestFolder(t)
-	data := []byte("\x0f\x00\x05\x00file1\x10\x00\x07\x00newname")
-	runProgram(t, makeCommandStream(BTRFS_SEND_C_RENAME, data))
-
-	stat, err := os.Stat(path.Join(testFolder, "newname"))
-	if os.IsNotExist(err) || stat.IsDir() {
-		t.Fatal("file1 was not renamed to newname")
-	}
-}
-
-func TestRmdirStream(t *testing.T) {
-	prepareTestFolder(t)
-	data := []byte("\x0f\x00\x04\x00dir1")
-	runProgram(t, makeCommandStream(BTRFS_SEND_C_RMDIR, data))
-
-	_, err := os.Stat(path.Join(testFolder, "newname"))
-	if !os.IsNotExist(err) {
-		t.Fatal("dir1 was not deleted")
-	}
-}
-
-func TestSymlinkStream(t *testing.T) {
-	prepareTestFolder(t)
-	data := []byte("\x0f\x00\x05\x00file2\x03\x00\x08\x00\xef\xcd\xab\x89\x67\x45\x23\x01\x11\x00\x05\x00file1")
-	runProgram(t, makeCommandStream(BTRFS_SEND_C_SYMLINK, data))
-
-	if runtime.GOOS == "windows" {
-		_, err := os.Stat(path.Join(testFolder, "file2.lnk"))
-		if os.IsNotExist(err) {
-			t.Fatal("file2 was not created")
-		}
-	} else {
-		stat, err := os.Lstat(path.Join(testFolder, "file2"))
-		if os.IsNotExist(err) {
-			t.Fatal("file2 was not created")
-		}
-		if stat.Mode()&os.ModeSymlink == 0 {
-			t.Fatal("file2 is not a symlink")
-		}
-	}
-}
-
-func TestTruncateStream(t *testing.T) {
-	prepareTestFolder(t)
-	data := []byte("\x0f\x00\x05\x00file1\x04\x00\x08\x00\x10\x00\x00\x00\x00\x00\x00\x00")
-	runProgram(t, makeCommandStream(BTRFS_SEND_C_TRUNCATE, data))
-
-	stat, _ := os.Stat(path.Join(testFolder, "file1"))
-	if stat.Size() != 16 {
-		t.Fatal("file1 was not truncated")
-	}
-	file1bytes, _ := ioutil.ReadFile(path.Join(testFolder, "file1"))
-	if string(file1bytes) != "The quick brown " {
-		t.Fatal("file1 was not truncated correctly")
-	}
-}
-
-func TestUnlinkStream(t *testing.T) {
-	prepareTestFolder(t)
-	data := []byte("\x0f\x00\x05\x00file1")
-	runProgram(t, makeCommandStream(BTRFS_SEND_C_UNLINK, data))
-
-	_, err := os.Stat(path.Join(testFolder, "file1"))
-	if !os.IsNotExist(err) {
-		t.Fatal("file1 was not deleted")
-	}
-}
-
-func TestWriteStream(t *testing.T) {
-	prepareTestFolder(t)
-	data := []byte("\x0f\x00\x05\x00file1\x12\x00\x08\x00\x04\x00\x00\x00\x00\x00\x00\x00\x13\x00\x05\x00QUICK")
-	runProgram(t, makeCommandStream(BTRFS_SEND_C_WRITE, data))
-
-	stat, _ := os.Stat(path.Join(testFolder, "file1"))
-	if stat.Size() != int64(len(quickBrownFox)) {
-		t.Fatal("size of file1 changed")
-	}
-	file1bytes, _ := ioutil.ReadFile(path.Join(testFolder, "file1"))
-	if string(file1bytes) != strings.ReplaceAll(quickBrownFox, "quick", "QUICK") {
-		t.Fatal("file1 was not written to correctly")
-	}
-}
-
 func simpleResolveLnkFile(pathToCheck string) string {
 	if runtime.GOOS != "windows" || !strings.HasSuffix(pathToCheck, ".lnk") {
 		return pathToCheck
@@ -253,7 +131,7 @@ func compareHashes(t *testing.T, hashSource string) {
 }
 
 func TestStream(t *testing.T) {
-	prepareTestFolder2(t, true)
+	prepareTestFolder(t, true)
 	data, _ := ioutil.ReadFile("data/stream01.bin")
 	runProgram(t, data)
 	compareHashes(t, "data/stream01_files.txt")
@@ -264,7 +142,7 @@ func TestStream(t *testing.T) {
 }
 
 func TestStreamWithCommandline(t *testing.T) {
-	prepareTestFolder2(t, true)
+	prepareTestFolder(t, true)
 	args := [...]string{programName, "-o", testFolder, "-i", "data/stream01.bin"}
 	Main(args[:])
 	compareHashes(t, "data/stream01_files.txt")
--- unit_test.go
+++ unit_test.go
@@ -3,6 +3,10 @@ package main
 import (
 	"bytes"
 	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"runtime"
 	"strings"
 	"testing"
 )
@@ -19,10 +23,12 @@ func testCommandAndExpectStdout(t *testing.T, command func(io.Reader, Config) bo
 	stdout := bytes.NewBufferString("")
 	dataReader := bytes.NewReader(data)
 	config := Config{
-		DryRun:  true,
+		Root:    testFolder,
+		DryRun:  false,
 		Verbose: true,
 		Stdout:  stdout,
 	}
+	prepareTestFolder(t, false)
 
 	returnValue := command(dataReader, config)
 	if !returnValue {
@@ -42,46 +48,110 @@ func testCommandAndExpectStdout(t *testing.T, command func(io.Reader, Config) bo
 }
 
 func TestLinkCommand(t *testing.T) {
-	data := []byte("\x0f\x00\x04\x00dir1\x11\x00\x04\x00dir2")
-	testCommandAndExpectStdout(t, linkCommand, data, "link dir1 to dir2\n")
+	data := []byte("\x0f\x00\x05\x00file2\x11\x00\x05\x00file1")
+	testCommandAndExpectStdout(t, linkCommand, data, "link file2 to file1\n")
+
+	stat1, _ := os.Stat(path.Join(testFolder, "file1"))
+	stat2, _ := os.Stat(path.Join(testFolder, "file2"))
+	if !os.SameFile(stat1, stat2) {
+		t.Fatal("file1 and file2 are not the same")
+	}
 }
 
 func TestMkdirCommand(t *testing.T) {
-	data := []byte("\x0f\x00\x04\x00dir1\x03\x00\x08\x00\xef\xcd\xab\x89\x67\x45\x23\x01")
-	testCommandAndExpectStdout(t, mkdirCommand, data, "mkdir dir1 (81985529216486895)\n")
+	data := []byte("\x0f\x00\x04\x00dir2\x03\x00\x08\x00\xef\xcd\xab\x89\x67\x45\x23\x01")
+	testCommandAndExpectStdout(t, mkdirCommand, data, "mkdir dir2 (81985529216486895)\n")
+
+	stat, err := os.Stat(path.Join(testFolder, "dir2"))
+	if os.IsNotExist(err) || !stat.IsDir() {
+		t.Fatal("dir2 was not created")
+	}
 }
 
 func TestMkfileCommand(t *testing.T) {
-	data := []byte("\x0f\x00\x05\x00file1\x03\x00\x08\x00\xef\xcd\xab\x89\x67\x45\x23\x01")
-	testCommandAndExpectStdout(t, mkfileCommand, data, "mkfile file1 (81985529216486895)\n")
+	data := []byte("\x0f\x00\x05\x00file2\x03\x00\x08\x00\xef\xcd\xab\x89\x67\x45\x23\x01")
+	testCommandAndExpectStdout(t, mkfileCommand, data, "mkfile file2 (81985529216486895)\n")
+
+	stat, err := os.Stat(path.Join(testFolder, "file2"))
+	if os.IsNotExist(err) || stat.IsDir() {
+		t.Fatal("file2 was not created")
+	}
 }
 
 func TestRenameCommand(t *testing.T) {
 	data := []byte("\x0f\x00\x05\x00file1\x10\x00\x07\x00newname")
 	testCommandAndExpectStdout(t, renameCommand, data, "rename file1 to newname\n")
+
+	stat, err := os.Stat(path.Join(testFolder, "newname"))
+	if os.IsNotExist(err) || stat.IsDir() {
+		t.Fatal("file1 was not renamed to newname")
+	}
 }
 
 func TestRmdirCommand(t *testing.T) {
 	data := []byte("\x0f\x00\x04\x00dir1")
 	testCommandAndExpectStdout(t, rmdirCommand, data, "rmdir dir1\n")
+
+	_, err := os.Stat(path.Join(testFolder, "newname"))
+	if !os.IsNotExist(err) {
+		t.Fatal("dir1 was not deleted")
+	}
 }
 
 func TestSymlinkCommand(t *testing.T) {
-	data := []byte("\x0f\x00\x05\x00file1\x03\x00\x08\x00\xef\xcd\xab\x89\x67\x45\x23\x01\x11\x00\x05\x00file2")
-	testCommandAndExpectStdout(t, symlinkCommand, data, "symlink file1 to file2 (81985529216486895)\n")
+	data := []byte("\x0f\x00\x05\x00file2\x03\x00\x08\x00\xef\xcd\xab\x89\x67\x45\x23\x01\x11\x00\x05\x00file1")
+	testCommandAndExpectStdout(t, symlinkCommand, data, "symlink file2 to file1 (81985529216486895)\n")
+
+	if runtime.GOOS == "windows" {
+		_, err := os.Stat(path.Join(testFolder, "file2.lnk"))
+		if os.IsNotExist(err) {
+			t.Fatal("file2 was not created")
+		}
+	} else {
+		stat, err := os.Lstat(path.Join(testFolder, "file2"))
+		if os.IsNotExist(err) {
+			t.Fatal("file2 was not created")
+		}
+		if stat.Mode()&os.ModeSymlink == 0 {
+			t.Fatal("file2 is not a symlink")
+		}
+	}
 }
 
 func TestTruncateCommand(t *testing.T) {
-	data := []byte("\x0f\x00\x05\x00file1\x04\x00\x08\x00\xef\xcd\xab\x89\x67\x45\x23\x01")
-	testCommandAndExpectStdout(t, truncateCommand, data, "truncate file1 to 81985529216486895 bytes\n")
+	data := []byte("\x0f\x00\x05\x00file1\x04\x00\x08\x00\x10\x00\x00\x00\x00\x00\x00\x00")
+	testCommandAndExpectStdout(t, truncateCommand, data, "truncate file1 to 16 bytes\n")
+
+	stat, _ := os.Stat(path.Join(testFolder, "file1"))
+	if stat.Size() != 16 {
+		t.Fatal("file1 was not truncated")
+	}
+	file1bytes, _ := ioutil.ReadFile(path.Join(testFolder, "file1"))
+	if string(file1bytes) != "The quick brown " {
+		t.Fatal("file1 was not truncated correctly")
+	}
 }
 
 func TestUnlinkCommand(t *testing.T) {
 	data := []byte("\x0f\x00\x05\x00file1")
 	testCommandAndExpectStdout(t, unlinkCommand, data, "unlink file1\n")
+
+	_, err := os.Stat(path.Join(testFolder, "file1"))
+	if !os.IsNotExist(err) {
+		t.Fatal("file1 was not deleted")
+	}
 }
 
 func TestWriteCommand(t *testing.T) {
-	data := []byte("\x0f\x00\x05\x00file1\x12\x00\x08\x00\xef\xcd\xab\x89\x67\x45\x23\x01\x13\x00\x07\x00\xcd\xab\x89\x67\x45\x23\x01")
-	testCommandAndExpectStdout(t, writeCommand, data, "write file1 (offset 81985529216486895, datalen 7)\n")
+	data := []byte("\x0f\x00\x05\x00file1\x12\x00\x08\x00\x04\x00\x00\x00\x00\x00\x00\x00\x13\x00\x05\x00QUICK")
+	testCommandAndExpectStdout(t, writeCommand, data, "write file1 (offset 4, datalen 5)\n")
+
+	stat, _ := os.Stat(path.Join(testFolder, "file1"))
+	if stat.Size() != int64(len(quickBrownFox)) {
+		t.Fatal("size of file1 changed")
+	}
+	file1bytes, _ := ioutil.ReadFile(path.Join(testFolder, "file1"))
+	if string(file1bytes) != strings.ReplaceAll(quickBrownFox, "quick", "QUICK") {
+		t.Fatal("file1 was not written to correctly")
+	}
 }
